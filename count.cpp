3967d40ba25a2273ff28e51e19b217871b86948a
XiangenDeng <xiangen@m.scnu.edu.cn>
2019-01-29 18:06:27 +0800

    Update LK

--- count.cpp
+++ count.cpp
@@ -1,46 +1,15 @@
 //-------------------------------------------------------
-//算法描述：目标检测计数算法
-//@update
-//2019-1-26 祥恩　重写算法，改用mog2.提速效果明显，但达不到实时要求
-//2019-1-26 祥恩  写LK光流算法，进度条20%　
-//2019-1-28 祥恩  写LK稀疏光流算法,进度60%,稠密光流运算量大
+//算法描述：目标检测计数算法,LK
 //-------------------------------------------------------
 
-
-//-----------------------------
-//    描述：包含头文件和命名空间
-//-----------------------------
-#include "opencv2/video/tracking.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc.hpp"
-#include  <opencv.hpp>
-#include  <iostream>
-using namespace std;
-using namespace cv;
-
-//-------------------------------
-//     描述：全局函数声明
-//-------------------------------
-bool addNewPoints();
-bool acceptTrackedPoint(int i);
-
-//---------------------------------
-// 描述：全局变量的定义
-//---------------------------------
-int thresholdValue=40;//二值化参数
-int maxThresholdValue=200;
-Mat currentFrame,preFrame;
-Mat curGrayFrame,preGrayFrame;
-vector<Point2f> points[2];
-vector<Point2f> trackPoints;
-vector<uchar> status;//输出状态vector
+#include "count.h"
 
 //主函数
 int main()
 {
     VideoCapture cap;
-    cap.open(0);
+    //cap.open(0);
+    cap.open("./nice10.avi");
     if(!cap.isOpened())
     {
         cout<<"unable to open video"<<endl;
@@ -53,22 +22,9 @@ int main()
         return -1;
     }
 
-    //LK函数参数
-    int maxLevel=3;//基于最大金字塔层次数
-    int winSize =15;//每级金字塔的搜索窗口大小
     vector<float> error;
-    //getFeatures参数
-    int cornerCount=150;//返回的最大角点数目
-    double qualityLevel=0.01;//角点可接受的最小特征值
-    double minDistance=10;//角点之间最小距离
-    int iteration=20;   //迭代次数
-    double epislon=0.1; //最小精度
-    TermCriteria termcrit(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS, iteration, epislon);//准则,迭代次数及精度
-
-    bool isDraw=true;
-
-    namedWindow("show",WINDOW_AUTOSIZE);
-    //createTrackbar("二值化","show",&thresholdValue,maxThresholdValue,onThreshold);
+    Point2f history;
+    namedWindow("show",WINDOW_NORMAL);
     while(true)
     {
         cap>>currentFrame;
@@ -78,31 +34,38 @@ int main()
             break;
         }
         cvtColor(currentFrame,curGrayFrame,CV_BGR2GRAY);
-        //onThreshold(thresholdValue,0);
+
+        //特征点太少则重新检测添加特征点
         if(addNewPoints())
         {
             //preGrayFrame=curGrayFrame.clone();
-            goodFeaturesToTrack(curGrayFrame,points[0],cornerCount,qualityLevel,minDistance);
-            cornerSubPix(curGrayFrame,points[0],cvSize(winSize,winSize),Size(-1,-1),termcrit);//精确检测角点函数
-            trackPoints.insert(trackPoints.end(),points[0].begin(),points[0].end());
+            goodFeaturesToTrack(curGrayFrame,featurePoints,cornerCount,qualityLevel,minDistance);
+            cornerSubPix(curGrayFrame,featurePoints,cvSize(winSize,winSize),Size(-1,-1),termcrit);//精确检测角点函数
+            points[0].insert(points[0].end(),featurePoints.begin(),featurePoints.end());
+            trackPoints.insert(trackPoints.end(),featurePoints.begin(),featurePoints.end());
         }
 
+        //第一帧
         if (preGrayFrame.empty())
         {
             curGrayFrame.copyTo(preGrayFrame);
         }
+
         //LK函数
         calcOpticalFlowPyrLK(preGrayFrame,curGrayFrame,points[0], points[1], status, error,Size(winSize,winSize),maxLevel,termcrit);
 
-        //去掉位置不变的点
+        //去掉位置不变的点且超过计数线的点
         int k=0;
         for(int i=0;i<points[1].size();i++)
         {
             if (acceptTrackedPoint(i))
             {
-                trackPoints[k] = trackPoints[i];
-                points[1][k] = points[1][i];
-                k++;
+                if(points[1][i].y<=countLine&&points[1][i].x<countLeft&&points[1][i].x>countRight)
+                {
+                    trackPoints[k] = trackPoints[i];
+                    points[1][k] = points[1][i];
+                    k++;
+                }
             }
         }
         points[1].resize(k);//重新调整容器大小
@@ -111,20 +74,49 @@ int main()
         //显示特征点和运动轨迹
         if(isDraw)
         {
+            line(currentFrame,Point(countRight,countLine),Point(countLeft,countLine),Scalar(255,0,0),2);//下边界
+            line(currentFrame,Point(countRight,0),Point(countRight,countLine),Scalar(255,0,0),2);//右边界
+            line(currentFrame,Point(countLeft,0),Point(countLeft,countLine),Scalar(255,0,0),2);//左边界
             for(int i=0;i<points[1].size();i++)
             {
-                line(currentFrame,trackPoints[i],points[1][i],Scalar(0,0,255),2);
+                //line(currentFrame,trackPoints[i],points[1][i],Scalar(0,0,255),2);
                 circle(currentFrame,points[1][i],3,Scalar(0,255,0),-1);
             }
         }
 
-        imshow("show",currentFrame);
+        //计数方法,连续通过n个点则计数加１
+        for(int i=0;i<points[1].size();i++)
+        {
+            //cout<<(int)points[1][i].y<<endl;
+            if((int)points[1][i].y==countLine-1)
+            {
+                if(history.y-points[1][i].y<1)
+                    passPoint+=1;
+                history=points[1][i];
+                if(passPoint>=15)
+                {
+                    number++;
+                    passPoint=0;
+                }
+                break;
+            }
+        }
+
+        //显示数量
+        sprintf(numText,"%d",number);
+        putText(currentFrame,numText,Point(60,50),FONT_HERSHEY_SIMPLEX,2,Scalar(0,0,255),4,8);//显示数量
+
+        swap(points[1],points[0]);
+        preGrayFrame=curGrayFrame.clone();
+
         cvResizeWindow("show",640,480);
+        imshow("show",currentFrame);
         if(waitKey(30)==27)
         {
             cout<<"quit"<<endl;
             break;
         }
+
     }
     cap.release();
     return 0;
@@ -134,11 +126,11 @@ int main()
 //检测是否添加新点
 bool addNewPoints()
 {
-	return points[0].size() <= 10;
+	return trackPoints.size() <= cornerCount;
 }
 
 //决定那些跟踪点被接受
 bool acceptTrackedPoint(int i)
 {
-	return status[i] && ((abs(points[0][i].x - points[1][i].x) + abs(points[0][i].y - points[1][i].y)) > 2);
+	return status[i] && ((abs(points[0][i].x - points[1][i].x) + abs(points[0][i].y - points[1][i].y)) > 0.5);
 }
--- count.h
+++ count.h
@@ -4,27 +4,62 @@
  *@update:
  *2019-1-21 祥恩　尝试用MobileNet&SSD深度学习模型进行目标检测,树莓派CPU占用率达９０％以上，无法运行
 　*2019-1-22 祥恩  换yolov3模型,比MobileNet&SSD还要慢．暂时放弃神经网络进行目标检测的想法
+ *2019-1-23 祥恩　工厂考察环境
  *2019-1-24 祥恩　重新帧间差分算法,规范代码
 　*2019-1-25 祥恩　重写帧差算法，去掉了没必要的图像处理步骤以及缩小图片尺寸
  *               在树莓派上速度有提升，但依旧延时严重
  *2019-1-26 祥恩　重写算法，改用mog2.提速效果明显，但达不到实时要求
  *2019-1-26 祥恩  写LK光流算法，进度条20%　
- *2019-1-28 祥恩  写LK稀疏光流算法,稠密光流运算量大
+ *2019-1-28 祥恩  写LK稀疏光流算法,稠密光流运算量大80%
+　*2019-1-29 祥恩  写lk算法，角点过多，且更新乱，计数难度大
  */
 #ifndef COUNT_H_INCLUDED
 #define COUNT_H_INCLUDED
 
-#include <opencv2/opencv.hpp>
-#include <iostream>
-using namespace cv;
+//-----------------------------
+//    描述：包含头文件和命名空间
+//-----------------------------
+#include "opencv2/video/tracking.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include  <opencv.hpp>
+#include  <iostream>
 using namespace std;
+using namespace cv;
 
-Mat moveDetect();//运动目标检测计数
-static void onValue(int,void*);
-//定义轮廓结构题
-struct  contourBlock{
-    double area;        //轮廓面积
-    Point2d mc;         //轮廓质心
-};
+//-------------------------------
+//     描述：全局函数声明
+//-------------------------------
+bool addNewPoints();
+bool acceptTrackedPoint(int i);
+
+
+//---------------------------------
+// 描述：全局变量的定义
+//---------------------------------
+Mat currentFrame,preFrame;
+Mat curGrayFrame,preGrayFrame;
+vector<Point2f> points[2];
+vector<Point2f> trackPoints;
+vector<Point2f> featurePoints;
+vector<uchar> status;//输出状态vector，特征点被成功跟踪的标志
+//LK函数参数
+int maxLevel=1;//基于最大金字塔层次数
+int winSize =15;//每级金字塔的搜索窗口大小
+//getFeatures参数
+int cornerCount=100;//返回的最大角点数目
+double qualityLevel=0.01;//角点可接受的最小特征值
+double minDistance=10;//角点之间最小距离
+int iteration=40;   //迭代次数
+double epislon=0.01; //最小精度
+TermCriteria termcrit(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS, iteration, epislon);//准则,迭代次数及精度
+bool isDraw=true;
+int countLine=280;
+int countRight=60;
+int countLeft=500;
+int passPoint=0;
+int number=0;
+char numText[10];
 
 #endif // COUNT_H_INCLUDED
